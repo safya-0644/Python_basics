ee3fe350fee232025e8c746e6c351487f03a0d8f
SAFY <safyaaleen.v@gmail.com>
2024-12-04 17:08:46 +0530

    Create Occurrence count.py

---
+++ Case Study 1/Occurrence count.py
@@ -0,0 +1,7 @@
+#Find the number of occurrences of a substring in a string
+
+# Define a string
+str_x = "Emma is good developer. Emma is a writer"
+
+# Count the number of occurrences of the substring "Emma" in the string
+print(str_x.count("Emma"))
