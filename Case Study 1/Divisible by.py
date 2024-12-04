b8f842958407dcfcde7c955bf7cf4e6656bcb2ab
SAFY <safyaaleen.v@gmail.com>
2024-12-04 17:07:27 +0530

    Create Divisible by.py

---
+++ Case Study 1/Divisible by.py
@@ -0,0 +1,11 @@
+#Display numbers divisible by 5
+
+# Define a list of numbers
+l = [10, 20, 33, 46, 55]
+
+# Loop through each number in the list
+for i in l:
+    # Check if the number is divisible by 5
+    if i % 5 == 0:
+        # Print the number if divisible by 5
+        print(i)
