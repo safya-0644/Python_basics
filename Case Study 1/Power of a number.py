1e29a0b734aeeaff4491ebdccbcfba56b3ead8e9
SAFY <safyaaleen.v@gmail.com>
2024-12-04 17:22:15 +0530

    Create Power of a number.py

---
+++ Case Study 1/Power of a number.py
@@ -0,0 +1,8 @@
+# Program to calculate the power of a number
+
+# Taking the base and exponent as input from the user
+b = int(input('Enter the base value: '))  # The base value
+e = int(input('Enter the exponent value: '))  # The exponent value
+
+# Printing the result of base raised to the power of exponent
+print(f'{b} raised to the power of {e}:', b**e)
