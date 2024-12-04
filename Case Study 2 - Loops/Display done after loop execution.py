7f7653140d47fa0736d6ed1da54bf9e340d1fc79
SAFY <safyaaleen.v@gmail.com>
2024-12-04 18:04:09 +0530

    Create Display done after loop execution.py

---
+++ Case Study 2 - Loops/Display done after loop execution.py
@@ -0,0 +1,6 @@
+#Display a message “Done” after the successful execution of the for loop
+
+for i in range(5):  # Loop will iterate over 0 to 4
+    print(i)  # Prints the current value of i
+else:
+    print('Done!')  # Prints 'Done!' after the loop completes
