88ba772f4a567d724a77c32551baa6a6a90f2a20
SAFY <safyaaleen.v@gmail.com>
2024-12-04 18:16:41 +0530

    Create Sideway pyramid.py

---
+++ Case Study 2 - Loops/Sideway pyramid.py
@@ -0,0 +1,7 @@
+# Upward Triangle
+for i in range(1, 5):  # Loop through numbers 1 to 4 to print the upward triangle
+  print(' '.join(['*'] * i))  # Print i asterisks with a space in between, forming a triangle
+
+# Downward Triangle
+for i in range(5, 0, -1):  # Loop through numbers 5 to 1 to print the downward triangle
+  print(' '.join(['*'] * i))  # Print i asterisks with a space in between, forming the opposite triangle
