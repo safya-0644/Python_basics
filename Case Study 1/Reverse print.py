944eb8cede5f354fcaafbf392d4fb25b35d30210
SAFY <safyaaleen.v@gmail.com>
2024-12-04 17:13:53 +0530

    Create Reverse print.py

---
+++ Case Study 1/Reverse print.py
@@ -0,0 +1,7 @@
+#Program to Reverse the Digits of a Number and Print
+
+# Get an input number as a string
+num = input("Enter a number: ")
+
+# Reverse the string and join the digits with spaces between them
+print(' '.join(num[::-1]))
