6d02ded5edb0c10de23eaf0427c4d9f1c051bdd5
SAFY <safyaaleen.v@gmail.com>
2024-12-04 13:43:32 +0530

    Update Age Verification.py

--- Case Study 3 - if else case/Age Verification.py
+++ Case Study 3 - if else case/Age Verification.py
@@ -1,19 +1,22 @@
 #Write a program that checks if a person is eligible to vote.
+
+#Getting values from the user
 data = input("Enter student details (Name,Age) separated by semicolons: ")
 pep_det = []
 people = data.split(';')  
 for person in people:
+    #Seperating values next to ,
     people_det = person.split(',')
     name = people_det[0]
     age = int(people_det[1])
-    
+    #Checking age condition
     if age >=18:
         eligibility = 'Eligible'
     else:
         eligibility = 'Not Eligible'
-    
+    #Updating the values
     pep_det.append([name, age , eligibility])
-
+#Print all the values
 print("\nFinal details:")
 for person in pep_det:
     print(person)
